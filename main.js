1325122a91daaaeccfbb9e66079a14bf2073bd92
BuildTools <totogonzalezfernandez@gmail.com>
2018-01-19 21:46:47 -0500

    woah das a big

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-window.onload = function() {
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        title: {
-            text: "Desktop Search Engine Market Share - 2016"
-        },
-        data: [{
-            type: "pie",
-            startAngle: 240,
-            yValueFormatString: "##0.00\"%\"",
-            indexLabel: "{label} {y}",
-            dataPoints: [
-                { y: 79.45, label: "Google" },
-                { y: 7.31, label: "Bing" },
-                { y: 7.06, label: "Baidu" },
-                { y: 4.91, label: "Yahoo" },
-                { y: 1.26, label: "Others" }
-            ]
-        }]
-    });
-    chart.render();
-
-}
+var time = 10; /* how long the timer runs for */
+var initialOffset = '440';
+var i = 1
+var interval = setInterval(function() {
+    $('.circle_animation').css('stroke-dashoffset', initialOffset - (i * (initialOffset / time)));
+    $('h2').text(i);
+    if (i == time) {
+        clearInterval(interval);
+    }
+    i++;
+}, 1000);
